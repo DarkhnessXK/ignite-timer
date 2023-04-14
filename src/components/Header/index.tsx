a85f89e8b65302953a905d36436d5759633ead6e
Giovanni Toledo <giovannitoledo@id.uff.br>
2023-04-14 12:37:01 -0300

    fix: css e icones

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
 import { HeaderContainer } from './styles'
+import { Timer, Scroll } from 'phosphor-react'
+import logoIgnite from '../../assets/Logo-ignite.svg'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <HeaderContainer>
-      <span>logo</span>
+      <img src={logoIgnite} alt=""></img>
       <nav>
-        <a href="">timer</a>
-        <a href="">history</a>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Historico">
+          <Scroll size={24} />
+        </NavLink>
       </nav>
     </HeaderContainer>
   )

3edc3ef4163cfb4c226cb959c80c3f20149f742e
Giovanni Toledo <giovannitoledo@id.uff.br>
2023-04-19 10:44:15 -0300

    fix: timer no title ao iniciar e reset( )

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -47,14 +47,17 @@ export function Home() {
   })
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
-
+  let interval: number
   useEffect(() => {
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
+    }
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
@@ -82,6 +85,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
 
   const task = watch('task')
 
